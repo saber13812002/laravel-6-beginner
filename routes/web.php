6975ec604a34e08a688ac8b894aeedf173232b81
Yusuke Sato <yusuke.sato@ys-office.me>
2019-09-24 17:51:49 +0000

    RESTful Controllers Part 1

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/about', 'HelloController@about');
 Route::get('/service', 'ServiceController@index');
 Route::post('/service', 'ServiceController@store');
+
+Route::get('/customers', 'CustomerController@index');
+Route::get('/customers/create', 'CustomerController@create');
+Route::post('/customers', 'CustomerController@store');
